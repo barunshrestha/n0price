b89a2e0da43eed86c7bf17d1a61d12ac24fc953a
bimal-tech <074bct107@nce.edu.np>
2022-09-19 09:35:15 +0545

    changes on routes

--- routes/web.php
+++ routes/web.php
@@ -29,37 +29,16 @@ Route::middleware(['auth', 'verified', 'topLevelApproval'])->group(function () {
     Route::get('/user/unnapprove/{id}', [UserController::class,'unapproveUser'])->name('user.unapprove');
     Route::post('/user/status', [UserController::class,'approvalFilter'])->name('user.approvalFilter');
     Route::resource('users', 'UserController');
-
     Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
 });
 
 
 Route::group(['middleware' => 'auth'], function () {
 
-    //Dahsboard - Reports
-
     Route::post('check_username', 'UserController@checkUsername')->name('check_username');
     Route::resource('users', 'UserController');
     Route::get('/reset_password/{id}', 'UserController@reset')->name('users.reset');
     Route::resource('roles', 'RoleController');
-    // Demo routes
-    Route::get('/datatables', 'PagesController@datatables');
-    Route::get('/ktdatatables', 'PagesController@ktDatatables');
-    Route::get('/select2', 'PagesController@select2');
-    Route::get('/jquerymask', 'PagesController@jQueryMask');
-    Route::get('/icons/custom-icons', 'PagesController@customIcons');
-    Route::get('/icons/flaticon', 'PagesController@flaticon');
-    Route::get('/icons/fontawesome', 'PagesController@fontawesome');
-    Route::get('/icons/lineawesome', 'PagesController@lineawesome');
-    Route::get('/icons/socicons', 'PagesController@socicons');
-    Route::get('/icons/svg', 'PagesController@svg');
-
-    Route::get('/systemMonitor', 'SystemMonitorController@status');
-
-    Route::get('/test', 'ServicingController@test');
-
-    // Quick search dummy route to display html elements in search dropdown (header search)
-    Route::get('/quick-search', 'PagesController@quickSearch')->name('quick-search');
 
     Route::get('verify-email', [EmailVerificationController::class, 'verify_email_invoke'])
         ->name('verification.notice');
