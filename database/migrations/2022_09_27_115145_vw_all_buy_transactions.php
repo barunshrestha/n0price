95951afeea21b785aa62b3e8b7d7c3df3e1ec689
bimal-tech <074bct107@nce.edu.np>
2022-09-27 17:40:41 +0545

    two new views added

---
+++ database/migrations/2022_09_27_115145_vw_all_buy_transactions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class VwAllBuyTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("drop view if exists vw_buy_transactions");
+        DB::statement("create view vw_buy_transactions as 
+        select transactions.id, transactions.user_id,transactions.units,
+        transactions.purchase_price,transactions.purchase_date,
+        coins.symbol,coins.name,coins.image
+        from transactions join coins on transactions.coin_id=coins.id 
+        where transactions.investment_type='buy'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_09_27_115200_vw_all_sell_transactions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class VwAllSellTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement("drop view if exists vw_sell_transactions");
+        DB::statement("create view vw_sell_transactions as 
+        select transactions.id, transactions.user_id,transactions.units,
+        transactions.purchase_price,transactions.purchase_date,
+        coins.symbol,coins.name,coins.image
+        from transactions join coins on transactions.coin_id=coins.id
+        where transactions.investment_type='sell'");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
