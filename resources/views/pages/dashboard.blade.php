085d8df9c1dff3e75f13868c7b873f2170e89f64
badri <badri305@gmail.com>
2022-10-10 15:51:15 +0545

    Portfolio summary

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -613,11 +613,11 @@
         //     });
         // }
 
-        // $.ajax({
-        //     url: "{{ route('portfolio_summary') }}",
-        //     success: function(result) {
-        //         $("#portfolio_summary").html(result);
-        //     }
-        // });
+        $.ajax({
+            url: "{{ route('portfolio_summary') }}",
+            success: function(result) {
+                $("#portfolio_summary").html(result);
+            }
+        });
     </script>
 @endsection
