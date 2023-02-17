f3465e1a6c5fa44d0eb42bb8d37d3fe6275e124d
bimal-tech <074bct107@nce.edu.np>
2023-02-17 14:36:22 +0545

    wallet address encrypted

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Portfolio extends Model
 {
@@ -16,4 +17,15 @@ class Portfolio extends Model
         'status',
         'wallet_address'
     ];
+
+    public function setWalletAddressAttribute($value)
+    {
+        $secret_key = (new Transaction)->secret_key;
+        $this->attributes['wallet_address'] = DB::raw("AES_ENCRYPT('$value', '$secret_key')");
+    }
+    public function getWalletAddressAttribute($value)
+    {
+        $secret_key = (new Transaction)->secret_key;
+        return (DB::select("select AES_DECRYPT('$value', '$secret_key') as dec_str"))[0]->dec_str;
+    }
 }
