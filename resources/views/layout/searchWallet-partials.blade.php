49f804079d84d20e1b24f58236a01c553d2152c9
root <root@noprice.co>
2023-03-14 03:48:53 +0000

    Changes in dashboard

--- resources/views/layout/searchWallet-partials.blade.php
+++ resources/views/layout/searchWallet-partials.blade.php
@@ -11,17 +11,17 @@
             font-weight: 600;
             font-size: 4em;
             letter-spacing: 0.3rem;">
-                Manage portfolios, not just coins...
+                manage portfolios, not just coins
             </h1>
             <div class="align-items-end" style="margin-left: 44.4%;margin-top:7em;">
-                <p class="text-muted">Show assets allocation and returns based on the risk factors</p>
+               <!-- <p class="text-muted">Show assets allocation and returns based on the risk factors</p>-->
             </div>
             <div class="d-flex flex-column">
                 <div class="d-flex justify-content-center">
                     <div class="input-icon mx-4">
                         <input type="text" name="wallet_address" class="form-control-lg form-control"
                             id="wallet_address_at_search" style="width: 45em;"
-                            placeholder="Search by ethereum addresses, separated by comma if multiple…"
+                            placeholder="search by ethereum addresses (max 5, separated by comma)"
                             value="{{ $address ?? '' }}">
                         <span>
                             <i class="flaticon2-search-1 text-muted"></i>
